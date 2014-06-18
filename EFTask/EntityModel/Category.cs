47482c589df5c8976be727f03c10efc38f92f127
worth-to-mention <worth.to.mention@gmail.com>
2014-06-18 09:30:51 +0400

    save

---
+++ EFTask/EntityModel/Category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTask.EntityModel
+{
+    public class Category
+    {
+        public Category()
+        {
+            Children = new HashSet<Category>();
+            Goods = new HashSet<Good>();
+        }
+
+        public Guid ID { get; set; }
+
+        public String Name { get; set; }
+
+        public Guid? Parent_Id { get; set; }
+
+        public virtual ICollection<Category> Children { get; private set; }
+
+        public virtual ICollection<Good> Goods { get; private set; }
+
+        public virtual Category Parent { get; set; }
+    }
+}
